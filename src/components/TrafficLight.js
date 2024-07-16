419a527c90eff0e401885741492471824d80a671
Marco Andres Urrutia Mora <maurrutiam@minsal.cl>
2024-07-16 18:49:50 -0400

    add const color

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import "./TrafficLight.css";
 
 export const TrafficLight = () => {
+    const [ color, setcolor ] = useState();
   return (
     <>
       <div className="top"></div>
       <div className="container">
-        <div className="light red"></div>
+        <div className="light red" onClick={e => {}}></div>
         <div className="light yellow"></div>
         <div className="light green"></div>
       </div>
