1b24879496e8fc521ece106c7cc5f281221ff5ff
Marco Andres Urrutia Mora <maurrutiam@minsal.cl>
2024-07-16 18:17:38 -0400

    estructura y diseño

---
+++ src/components/TrafficLight.js
@@ -0,0 +1,14 @@
+import "./TrafficLight.css";
+
+export const TrafficLight = () => {
+  return (
+    <>
+      <div className="top"></div>
+      <div className="container">
+        <div className="light red"></div>
+        <div className="light yellow"></div>
+        <div className="light green"></div>
+      </div>
+    </>
+  );
+};
