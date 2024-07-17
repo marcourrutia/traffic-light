59f62db86dcafd5af32998ffd7fee2027cc09a49
Marco Andres Urrutia Mora <maurrutiam@minsal.cl>
2024-07-17 13:20:37 -0400

    proyecto terminado

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -2,15 +2,39 @@ import { useState } from "react";
 import "./TrafficLight.css";
 
 export const TrafficLight = () => {
-    const [ color, setcolor ] = useState();
+  const [colorActivo, setColorActivo] = useState(null);
+  const cambiarColor = (color) => {
+    setColorActivo(color);
+  };
+  const alternarColor = () => {
+    if (colorActivo === null) {
+      setColorActivo("red");
+    } else if (colorActivo === "red") {
+      setColorActivo("yellow");
+    } else if (colorActivo === "yellow") {
+      setColorActivo("green");
+    } else if (colorActivo === "green") {
+      setColorActivo(null);
+    }
+  };
   return (
     <>
       <div className="top"></div>
       <div className="container">
-        <div className="light red" onClick={e => {}}></div>
-        <div className="light yellow"></div>
-        <div className="light green"></div>
+        <div
+          className={`light red ${colorActivo === "red" ? "selected" : ""}`}
+          onClick={() => cambiarColor("red")}
+        ></div>
+        <div
+          className={`light yellow ${colorActivo === "yellow" ? "selected" : ""}`}
+          onClick={() => cambiarColor("yellow")}
+        ></div>
+        <div
+          className={`light green ${colorActivo === "green" ? "selected" : ""}`}
+          onClick={() => cambiarColor("green")}
+        ></div>
       </div>
+      <button onClick={alternarColor}>Alternar luces</button>
     </>
   );
 };
